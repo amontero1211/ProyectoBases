847d6e35ecd695edcdf4fe5c95431d03471347ed
amontero1211 <amontero1211@gmail.com>
2022-09-09 19:48:30 -0400

    Header 2.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,26 @@
 import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
 
 function Header() {
   return (
-    <Box sx={{ display: 'flex', gap: 30 }}>
-      <Avatar alt="Museo de Arte Contemporaneo" src="src/pictures/Logo Museo.png" sx={{ width: 100, height: 100 }} />
-      <Box sx={{
-        display: 'flex',
-        gap: 2,
-        marginTop: 3,
-        marginBottom: 6,
-      }}
-      >
-        <p> Welcome! Join us</p>
-        <Button variant="contained" color="secondary">Register</Button>
-        <Button variant="outlined" color="secondary">Log In</Button>
+    <AppBar position="static" style={{ background: '#ce93d8' }}>
+      <Box sx={{ display: 'flex', gap: 30 }}>
+        <img src="src/pictures/Museo.png" alt="Museo de Arte Contemporaneo" height={100} width={100} className="logo" />
+        <Box sx={{
+          display: 'flex',
+          gap: 2,
+          marginTop: 3,
+          marginBottom: 6,
+          marginRight: 6,
+        }}
+        >
+          <p> Welcome! Join us</p>
+          <Button variant="contained" color="secondary">Register</Button>
+          <Button variant="outlined" color="secondary">Log In</Button>
+        </Box>
       </Box>
-    </Box>
+    </AppBar>
   );
 }
 
